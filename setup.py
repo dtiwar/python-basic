415389e782296f7a2c55e8fbe8d24ba81dafff08
deep <dtiwar@gmail.com>
2022-08-14 10:25:19 +0200

    updated steup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
+'''
+This is sample setup.py
+
+'''
 from pathlib import Path
-from importlib.resources import read_text
 import setuptools
 
 setuptools.setup(
-
     name="python-basic",
     version=1.0,
-    long_description=Path("README.md").read_text(),
+    long_description=Path("README.md").read_text(encoding='utf-8'),
     packages=setuptools.find_packages(exclude=["tests", "data"])
 )
